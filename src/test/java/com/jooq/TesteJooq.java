7c1bfb121ed96caf0b96a0191bfcc2a2dffb9714
Ramon <r_vieira5@hotmail.com>
2018-02-09 19:31:21 -0200

    Inicializando ambiente de testes
    A biblioteca usada para testes será o jUnit versão 5

---
+++ src/test/java/com/jooq/TesteJooq.java
@@ -0,0 +1,13 @@
+package com.jooq;
+
+import org.junit.jupiter.api.Test;
+
+public class TesteJooq {
+
+	@Test
+	public void testeInicial() {
+		System.out.println("jOOQ + Java 8");
+		System.out.println("https://www.jooq.org/doc/3.5/manual/getting-started/jooq-and-java-8/");
+	}
+
+}
