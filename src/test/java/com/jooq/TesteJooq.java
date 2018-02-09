3de043a26ee0b25b93f72c0581a41d998e2c90ff
Ramon <r_vieira5@hotmail.com>
2018-02-09 20:57:06 -0200

    Testando execução de select simples com o jOOQ

--- src/test/java/com/jooq/TesteJooq.java
+++ src/test/java/com/jooq/TesteJooq.java
@@ -1,13 +1,43 @@
 package com.jooq;
 
+import static org.jooq.h2.generated.Tables.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.function.Consumer;
+
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 import org.junit.jupiter.api.Test;
 
 public class TesteJooq {
 
 	@Test
-	public void testeInicial() {
+	public void testeInicial() throws SQLException {
 		System.out.println("jOOQ + Java 8");
 		System.out.println("https://www.jooq.org/doc/3.5/manual/getting-started/jooq-and-java-8/");
+
+		System.out.println();
+		System.out.println("Listando registros da tabela de livros:");
+		Connection conn = getConnection();
+		DSLContext create = DSL.using(conn, SQLDialect.MYSQL);
+		create. //
+				select().from(BOOK) //
+				.fetch() //
+				.forEach(System.out::println);
+	}
+
+	public Connection getConnection() throws SQLException {
+		String userName = "root";
+		String password = "admin";
+		String url = "jdbc:h2:./sql_schema/jooq-test";
+
+		Connection conn = DriverManager.getConnection(url, userName, password);
+		return conn;
 	}
 
 }
